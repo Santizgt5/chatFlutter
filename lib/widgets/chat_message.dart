c5abacb1255fb169b9ab45bcb62ae85ec406bb76
Santizgt5 <santiagoalvarive@gmail.com>
2021-05-15 21:23:22 -0500

    Maquetación chat terminada

---
+++ lib/widgets/chat_message.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+  final String texto;
+  final String uid;
+  final AnimationController animationController;
+
+  const ChatMessage({Key key, this.texto, this.uid, this.animationController}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: animationController,
+      child: SizeTransition(
+        sizeFactor: CurvedAnimation(parent: animationController, curve: Curves.easeInOut),
+        child: Container(
+          child: this.uid == '123' ? _myMessage() : _notMyMessage(),
+        ),
+      ),
+    );
+  }
+
+  Widget _myMessage() {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        margin: EdgeInsets.only(right: 7, bottom: 5, left: 50),
+        child: Text(
+          this.texto,
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+        decoration: BoxDecoration(
+            color: Color(0xff4D9EF6), borderRadius: BorderRadius.circular(20)),
+      ),
+    );
+  }
+
+  Widget _notMyMessage() {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        margin: EdgeInsets.only(left: 7, bottom: 5, right: 50),
+        child: Text(
+          this.texto,
+          style: TextStyle(color: Colors.black87, fontSize: 18),
+        ),
+        decoration: BoxDecoration(
+            color: Color(0xffE4E5E8), borderRadius: BorderRadius.circular(20)),
+      ),
+    );
+  }
+}
